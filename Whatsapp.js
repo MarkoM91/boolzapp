561b1c55a5aaa8d959b848d7031103075a7da78c
MarkoM91 <markomedic91@gmail.com>
2019-03-11 13:35:41 +0100

    commit6

--- Whatsapp.js
+++ Whatsapp.js
@@ -1,5 +1,64 @@
-function init() {
+function testAddMessage1() {
 
+var wrapper = $(".wrap");
+
+var message = document.createElement("div");
+var messageContent = document.createElement("p");
+var messageDetail = document.createElement("small");
+
+$(message).addClass("message");
+$(message).addClass("sent");
+
+$(messageContent).text("messagio dinamico");
+$(messageDetail).text("dettagli messaggio dinamico");
+
+message.append(messageContent);
+message.append(messageDetail);
+
+wrapper.append(message);
+}
+
+function testAddMessage2() {
+
+var wrapper = $(".wrapper");
+
+var message = document.createElement("div");
+var messageContent = document.createElement("p");
+var messageDetail = document.createElement("small");
+
+$(message).addClass("message");
+$(message).addClass("received");
+
+$(messageContent).text("messagio dinamico");
+$(messageDetail).text("dettagli messaggio dinamico");
+
+message.append(messageContent);
+message.append(messageDetail);
+
+wrapper.append(message);
+}
+
+function txtEnterEvent(e){
+  var keyPress = e.which;
+  if (keyPress == 13) {
+   testAddMessage2();
+  }
+}
+
+function init() {
+var btn1 = $("#myBtn1");
+btn1.click(function() {
+
+  testAddMessage1();
+});
+var btn2 = $("#myBtn2");
+btn2.click(function() {
+
+  testAddMessage2();
+});
+
+var myTxt = $("#myInput");
+myTxt.keyup();
 
 }
 
